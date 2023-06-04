c1295ed58b8ac532132b166da8549216a426917c
hu <337502348@qq.com>
2023-06-04 17:36:30 +0800

    提交

--- packages/module-a/lib/module-a.js
+++ packages/module-a/lib/module-a.js
@@ -1,14 +1,7 @@
-// module-a
 'use strict';
-const moduleB = require('module-b');
-console.log('moduleB:', moduleB());
-
-
-
 
 module.exports = moduleA;
 
-
 function moduleA() {
-    return "it's module a";
+    // TODO
 }
